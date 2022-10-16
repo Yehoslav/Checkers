e4715adb74d9c45a4d2a53c0bd31a651870609a1
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-10-16 20:23:34 +0300

    moved the functions around to prepare for websockets

--- main.js
+++ main.js
@@ -1,41 +1,59 @@
-var drawnBoard = []
+function createBoard() {
+  var drawnBoard = []
 
-for (var file = 0; file < 8; file++) {
-  for (var rank = 0; rank < 8; rank++) {
-    drawnBoard.push({
-      rank: rank,
-      file: file,
-      x: rank * 100,
-      y: file * 100,
-      width: 100,
-      height: 100,
-      fillcolor: (rank + file) % 2 == 0 ? "white" : "black",
-      cellType: (rank + file) % 2 == 0 ? "white" : "black",
-      isFilled: true
-    })
+  for (var file = 0; file < 8; file++) {
+    for (var rank = 0; rank < 8; rank++) {
+      drawnBoard.push({
+        rank: rank,
+        file: file,
+        x: rank * 100,
+        y: file * 100,
+        width: 100,
+        height: 100,
+        fillcolor: (rank + file) % 2 == 0 ? "white" : "black",
+        cellType: (rank + file) % 2 == 0 ? "white" : "black",
+        isFilled: true
+      })
+    }
   }
+
+  return drawnBoard;
 }
 
-function renderBoard(board, checkers) {
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
+  function hit(rect, x, y) {
+    return (x >= rect.x && x <= rect.x + rect.width && y >= rect.y && y <= rect.y + rect.height);
+  }
+
+  function handleMouseDown(e, rects, checkers) {
+    e.preventDefault();
   var $canvas = $("#canvas");
   var canvasOffset = $canvas.offset();
   var offsetX = canvasOffset.left;
   var offsetY = canvasOffset.top;
+    mouseX = parseInt(e.clientX - offsetX);
+    mouseY = parseInt(e.clientY - offsetY);
+    for (var i = 0; i < rects.length; i++) {
+      var rect = rects[i];
+      if (hit(rect, mouseX, mouseY)) {
+        rect.fillcolor = rect.fillcolor == "red" ? rect.cellType : "red"
+      } else {
+        rect.fillcolor = rect.cellType
+    }
+    }
+    draw(rects, checkers);
+  }
 
-  draw(board);
-
-  function draw() {
+  function draw(board, checkers) {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < drawnBoard.length; i++) {
-      var rect = drawnBoard[i];
+    for (var i = 0; i < board.length; i++) {
+      var rect = board[i];
       ctx.fillStyle = rect.fillcolor;
       ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
       ctx.strokeStyle = "black";
       ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
       ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
-
     }
     // Draw checkers
     for (var checker of checkers) {
@@ -54,36 +72,26 @@ function renderBoard(board, checkers) {
     }
   }
 
-  function hit(rect, x, y) {
-    return (x >= rect.x && x <= rect.x + rect.width && y >= rect.y && y <= rect.y + rect.height);
-  }
 
-  function handleMouseDown(e, rects) {
-    e.preventDefault();
-    mouseX = parseInt(e.clientX - offsetX);
-    mouseY = parseInt(e.clientY - offsetY);
-    for (var i = 0; i < drawnBoard.length; i++) {
-      var rect = drawnBoard[i];
-      if (hit(rect, mouseX, mouseY)) {
-        rect.fillcolor = rect.fillcolor == "red" ? rect.cellType : "red"
-      }
-    }
-    draw();
-  }
+function renderBoard(board, checkers) {
+  draw(board, checkers);
+
   $("#canvas").mousedown(function(e) {
-    handleMouseDown(e);
+    handleMouseDown(e, board, checkers);
   });
 };
 
-$(window).load(() => renderBoard(drawnBoard, [
-  {
-    type: "white",
-    file: 0, rank: 0
-  },
-  {
-    type: "black",
-    file: 2, rank: 2
-  }
-]))
-
+window.addEventListener("DOMContentLoaded", () => {
+  // Initialize the UI.
+  const board = createBoard();
+  renderBoard(board, [
+    {type: "black", rank: 2, file: 2},
+    {type: "white", rank: 0, file: 0}
+  ])
+  // Open the WebSocket connection and register event handlers.
+  // const websocket = new WebSocket("ws://192.168.1.6:8001/");
+  // initGame(websocket);
+  // receiveMoves(board, websocket);
+  // sendMoves(board, websocket);
+});
 
